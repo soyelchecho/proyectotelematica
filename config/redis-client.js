81673131bdba9cd11eefdbb23f9e9f270ce6b2ea
oigreslol <sergiorojas12ocm@gmail.com>
2019-09-15 23:47:50 -0500

    cambiando configuracion redis

--- config/redis-client.js
+++ config/redis-client.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
 const {promisify} = require('util');
-const client = redis.createClient();
+const client = redis.createClient(  {  host: 'redis-server',
+    port: 6379});
 
 module.exports = {cliente : client};
